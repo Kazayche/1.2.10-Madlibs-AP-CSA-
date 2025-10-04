3d7286a810451e2c5be5bc4fdb0432fe7d9af127
Shourya Pattabhi <96268277+Kazayche@users.noreply.github.com>
2025-10-03 20:52:44 -0700

    Update MadLib.java

--- MadLib.java
+++ MadLib.java
@@ -1,16 +1,15 @@
 import java.util.Scanner;
 
-// This program creates a Mad Libs story by collecting user inputs
-// and inserting them into a story template
+// Madlib code where user inputs get placed in our story template which is shown further down the code file 
 public class MadLib
 {
   public static void main(String[] args)
   {
-    Scanner input = new Scanner(System.in);
+    Scanner input = new Scanner(System.in);   //intializing the scanner use
     
     //  user inputs
-    System.out.print("Enter an adjective: ");
-    String adjective = input.nextLine();
+    System.out.print("Enter an adjective: ");     // asks user the question wich in this case asks for an adjective 
+    String adjective = input.nextLine();  //takes the scanner input of what the user says next 
     
     System.out.print("Enter a noun: ");
     String noun = input.nextLine();
@@ -31,7 +30,7 @@ public class MadLib
     String noun2 = input.nextLine();
     
     //  display the story
-    System.out.println("\nYour Mad Lib Story:");
-    System.out.println("Once upon a time in a " + adjective + " land, a " + noun + " began to " + verb + ". The " + pluralNoun + " watched as " + number + " " + noun2 + "s danced " + adverb + ".");
+    System.out.println("\nYour Mad Lib Story:");    //prints a result of the MadLib story given the user inputs 
+    System.out.println("Once upon a time in a " + adjective + " land, a " + noun + " began to " + verb + ". The " + pluralNoun + " watched as " + number + " " + noun2 + "s danced " + adverb + ".");  // combines the story template and the user inputs and prints it out at the end 
   }
 }
