c0c02a2c4ca51c7c8d25edfa95dca613faf1ee1b
Shourya Pattabhi <96268277+Kazayche@users.noreply.github.com>
2025-10-10 19:54:56 -0700

    Update Matlib_input.java

--- Matlib_input.java
+++ Matlib_input.java
@@ -19,25 +19,37 @@ public class MadLibsInputStory {
         System.out.print("Enter a NOUN: ");
         String noun = input.nextLine();
         
-        System.out.print("Enter an ADVERB: ");
-        String adverb = input.nextLine();
+       
         
-        System.out.print("Enter a FEELING: ");
-        String feeling = input.nextLine();
-        
-        System.out.println("\nEnter your template (use [NAME], [PLACE], [VERB], [ADJECTIVE], [NOUN], [ADVERB], [FEELING]):");
+        System.out.println("\nEnter your template (use [NAME], [PLACE], [VERB], [ADJECTIVE], [NOUN]:");
         String template = input.nextLine();
+        String story = template;
+        int index; 
+
+        while ((index=story.indexOf("[NAME]")) !=-1 {   // replaces name in the template the user inputted above. It would run through the input to find the name and returns -1 if it does not find it 
+            story=story.substring(0,index) + name + story.substring.(index+6); // the (0,index) is everything before the place of the [NAME] and the following (index+6) is everything after the [NAME]
+            // it would then replace it in the middle of before and after essientaly replacing it in the template
+        }
+
+        while ((index=story.indexOf("[PLACE]")) != -1 {
+            story=story.substring(0,index) + place + story.substring(index+7);
+        }
+
+        while ((index=story.indexOf("[VERB]")) != -1{
+            story=story.substring(0,index) + verb + story.substring(index+6);
+        }
+
+        while ((index = story.indexOf("[ADJECTIVE]")) != -1) {
+            story = story.substring(0, index) + adjective + story.substring(index + 11);
+        }
         
-        String story = template.replace("[NAME]", name)
-                               .replace("[PLACE]", place)
-                               .replace("[VERB]", verb)
-                               .replace("[ADJECTIVE]", adjective)
-                               .replace("[NOUN]", noun)
-                               .replace("[ADVERB]", adverb)
-                               .replace("[FEELING]", feeling);
-        
-        System.out.println("\n=== YOUR MAD LIB ===");
+        while ((index = story.indexOf("[NOUN]")) != -1) {
+            story = story.substring(0, index) + noun + story.substring(index + 6);
+        }
+
+        System.out.println(" Here is your madlib");
         System.out.println(story);
+     
         
         input.close();
     }
