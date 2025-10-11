6dac58ef962f813236c274c2741184e690c4b13a
Shourya Pattabhi <96268277+Kazayche@users.noreply.github.com>
2025-10-10 20:06:38 -0700

    Update MadLib.java

--- MadLib.java
+++ MadLib.java
@@ -8,24 +8,30 @@ public class MadLib
     Scanner input = new Scanner(System.in);   //intializing the scanner use
     
     //  user inputs
-    System.out.print("Enter an adjective: ");     // asks user the question wich in this case asks for an adjective 
-    String adjective = input.nextLine();  //takes the scanner input of what the user says next 
-    
+  
     System.out.print("Enter a noun: ");
     String noun = input.nextLine();
-    
-    System.out.print("Enter a verb: ");
-    String verb = input.nextLine();
-    
-    System.out.print("Enter a plural noun: ");
-    String pluralNoun = input.nextLine();
     
     System.out.print("Enter a number: ");
     String number = input.nextLine();
 
+    System.out.print("Enter a plural noun: ");
+    String pluralNoun = input.nextLine();
+    
+    
+    System.out.print("Enter a verb: ");
+    String verb = input.nextLine();
+    
+   
+    
+  
+    System.out.print("Enter an adjective: ");     // asks user the question wich in this case asks for an adjective 
+    String adjective = input.nextLine();  //takes the scanner input of what the user says next 
+    
+
     
     //  display the story
     System.out.println("\nYour Mad Lib Story:");    //prints a result of the MadLib story given the user inputs 
-    System.out.println("My" + adjective +"friend decided to" +verb + number + pluralNoun +"at the park. I couldn't believe my eyes when I saw them trying to lift the" + noun + "!");  // combines the story template and the user inputs and prints it out at the end 
+    System.out.println("My" + adjective + "friend decided to" + verb + number + pluralNoun +"at the park. I couldn't believe my eyes when I saw them trying to lift the" + noun + "!");  // combines the story template and the user inputs and prints it out at the end 
   }
 }
